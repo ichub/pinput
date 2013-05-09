5536d6a6c8b5f1b55023bb05fcb996ae64a0981f
Ivan Chub <subsbox@hotmail.com>
2013-05-09 14:52:03 -0700

    added unconventional characters

--- pinput.js
+++ pinput.js
@@ -4,6 +4,8 @@ for (var i = 0; i < 256; i++) {
 }
 
 document.onkeydown = function(e) {
+	if (e.which == 18)
+		e.preventDefault();
 	keyStates[e.which] = true;
 };
 
@@ -11,13 +13,59 @@ document.onkeyup = function(e) {
 	keyStates[e.which] = false;
 };
 
+function removeWhiteSpace(string) {
+	return string.replace(/\s+/, '')
+}
+
+function stripWhiteSpace(string) {
+	return string.replace(/\s+/, ' ')
+}
+
 function convertStringToKeycode(key) {
-	var key = key.toUpperCase();
+	key = removeWhiteSpace(key);
+	key = key.toUpperCase();
+
+	switch(key) {
+		case "BACKSPACE":
+			return 8;
+		case "TAB":
+			return 9;
+		case "ENTER":
+			return 13;
+		case "SHIFT":
+			return 16;
+		case "CONTROL":
+			return 17;
+		case "ALT":
+			return 18;
+		case "CAPSLOCK":
+			return 20;
+		case "ESCAPE":
+			return 27;
+		case "PAGEUP":
+			return 33;
+		case "PAGEDOWN":
+			return 34;
+		case "ARROWLEFT":
+			return 37;
+		case "ARROWUP":
+			return 38;
+		case "ARROWRIGHT":
+			return 38;
+		case "ARROWDOWN":
+			return 40;
+		case "INSERT": 
+			return 45;
+		case "DELETE":
+			return 46;
+	}
 	return key.charCodeAt(0);
 }
 
 function convertStringToKeyCombo(keyComboString) {
+	keyComboString = stripWhiteSpace(keyComboString);
 	var combo = keyComboString.split(' ');
+
 	for (var i = 0; i < combo.length; i++) {
 		combo[i] = convertStringToKeycode(combo[i]);
 	};
@@ -42,8 +90,8 @@ function isKeyComboDown(keyCombo) {
 }
 
 function dothing() {
-	if (isKeyComboDown('a b')) {
-		console.log("DUN");
+	if (isKeyComboDown('alt control f')) {
+		console.log('asd');
 	}
 }
 
